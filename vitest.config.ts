2d0125013aa8d93cd8ad9a1dc9b14dda139901cd
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-07-25 08:19:07 +0700

    chore: update package info

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    environment: "jsdom",
     coverage: {
       include: ["src/**/*.ts"],
-      exclude: ["src/index.ts"],
     },
   },
   plugins: [
